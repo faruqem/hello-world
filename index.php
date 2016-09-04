ba9389aaf517e98f199a8326cf1c6c117b6b8c38
Mohiuddin Faruqe <faruqem@yahoo.com>
2016-09-03 20:32:07 -0400

    Update index file.

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1>Hello World!</h1>
+<!doctype html>
+<html>
+<head>
+
+	<title>Hello World</title>
+	<meta charset='utf-8'>
+
+</head>
+<body>
+  <h1>Hello World!</h1>
+	<?php
+	echo 'Hello World! You are visiting ';
+	echo $_SERVER['SERVER_NAME'];
+	?>
+
+</body>
+</html>
